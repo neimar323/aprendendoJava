c72ffa692d23c960ef7b89ce362453182e62ecdc
Keoma <you@email.com>
2020-01-07 19:55:59 -0300

    commit inicial

---
+++ src/main/java/com/neimarRevolution/lucifer/LuciferApplication.java
@@ -0,0 +1,13 @@
+package com.neimarRevolution.lucifer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuciferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuciferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neimarRevolution/lucifer/LuciferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neimarRevolution.lucifer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuciferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
